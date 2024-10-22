fc22a1f59d1418dae41c8acf906ea497a78a6ae8
szymonjakubiec <99356886+szymonjakubiec@users.noreply.github.com>
2024-10-22 21:23:05 +0200

    Przywrócenie walidacji przy logowaniu się
    i poprawa nazwy Server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const mysql = require('mysql2/promise');  // Używamy mysql2 z obsługą obietnic
+const express = require("express");
+const mysql = require("mysql2/promise"); // Używamy mysql2 z obsługą obietnic
 const app = express();
 
 // Konfiguracja połączenia z MySQL na Azure
 const config = {
-  host: 'rateem-server.mysql.database.azure.com',
-  user: 'GodAdmin',
-  password: 'ZAQ!2wsx',
-  database: 'ratem',
-  port: 3306,  // Użyj poprawnego portu dla MySQL
-  ssl: {      // Azure wymaga SSL
-    rejectUnauthorized: true
-  }
+  host: "rateem-server.mysql.database.azure.com",
+  user: "GodAdmin",
+  password: "ZAQ!2wsx",
+  database: "ratem",
+  port: 3306, // Użyj poprawnego portu dla MySQL
+  ssl: {
+    // Azure wymaga SSL
+    rejectUnauthorized: true,
+  },
 };
 
-
 // Endpoint API
-app.get('/api/dane', async (req, res) => {
+app.get("/api/dane", async (req, res) => {
   let connection;
   try {
     // Połączenie z bazą danych
     connection = await mysql.createConnection(config);
-    const [rows, fields] = await connection.execute('SELECT * FROM oceny');
-    
+    const [rows, fields] = await connection.execute("SELECT * FROM oceny");
+
     // Zwracanie danych do klienta
     res.json(rows);
   } catch (err) {
@@ -34,19 +34,21 @@ app.get('/api/dane', async (req, res) => {
       try {
         await connection.end();
       } catch (err) {
-        console.error('Błąd podczas zamykania połączenia:', err.message);
+        console.error("Błąd podczas zamykania połączenia:", err.message);
       }
     }
   }
 });
 
-app.get('/api/uzytkownicy', async (req, res) => {
+app.get("/api/uzytkownicy", async (req, res) => {
   let connection;
   try {
     // Połączenie z bazą danych
     connection = await mysql.createConnection(config);
-    const [rows, fields] = await connection.execute('SELECT email, haslo FROM uzytkownicy');
-    
+    const [rows, fields] = await connection.execute(
+      "SELECT email, haslo FROM uzytkownicy"
+    );
+
     // Zwracanie danych do klienta
     res.json(rows);
   } catch (err) {
@@ -58,11 +60,13 @@ app.get('/api/uzytkownicy', async (req, res) => {
       try {
         await connection.end();
       } catch (err) {
-        console.error('Błąd podczas zamykania połączenia:', err.message);
+        console.error("Błąd podczas zamykania połączenia:", err.message);
       }
     }
   }
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
