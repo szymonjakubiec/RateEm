fecb34edba610b52fc6a95745e39865ca4063c61
szymonjakubiec <99356886+szymonjakubiec@users.noreply.github.com>
2024-10-25 00:18:37 +0200

    Przechodzenie do profile screen z wybranym politykiem
    wybranym z dropdowna

--- src/main/search/ProfileScreen.jsx
+++ src/main/search/ProfileScreen.jsx
@@ -1,21 +1,29 @@
 import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer, useRoute } from "@react-navigation/native";
 
-export default function ProfileScreen(){
-    return(
-        <View style={styles.container}>
-            <Text>To jest profil wybranego polityka.</Text>
-        </View>
-    );
+export default function ProfileScreen({ navigation }) {
+  const route = useRoute();
+  const selectedPolitician =
+    route.params?.data[route.params?.selectedPolitician - 1].value;
+  route.params?.selectedPolitician;
+
+  function print() {
+    console.log(selectedPolitician);
+  }
+  return (
+    <View style={styles.container}>
+      <Text>To jest profil {selectedPolitician}.</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: 70,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 70,
+  },
 });
--- src/main/search/SearchScreen.jsx
+++ src/main/search/SearchScreen.jsx
@@ -1,39 +1,72 @@
 import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from "@react-navigation/native";
+import { SearchBar } from "react-native-elements";
+import { useEffect, useState } from "react";
+import { SelectList } from "react-native-dropdown-select-list";
 
-export default function SearchScreen({ navigation }){
-    return(
-        <View style={styles.container}>
-            <Text>To jest wyszukiwarka.</Text>
-            <TouchableHighlight
-                style={styles.button}
-                onPress={() => {navigation.navigate('Profile')}}
-            >
-                <Text style={styles.searchText}>Wyszukaj polityka</Text>
-            </TouchableHighlight>
-        </View>
-        
-    );
+export default function SearchScreen({ navigation }) {
+  const [searchText, setSearchText] = useState("");
+  const [selectedPolitician, setSelectedPolitician] = useState("");
+
+  const data = [
+    { key: "1", value: "Korwinedes z Myken" },
+    { key: "2", value: "Grzesio Braun" },
+    { key: "3", value: "Donald Tusk" },
+    { key: "4", value: "Jarek Kaczyński" },
+    { key: "5", value: "Sławek Memcen" },
+    { key: "6", value: "Szymon Kotłownia" },
+    { key: "7", value: "Al Asad" },
+  ];
+  return (
+    <View style={styles.container}>
+      <SelectList
+        data={data}
+        placeholder="Wyszukaj polityka"
+        searchPlaceholder="Wyszukaj polityka"
+        setSelected={setSelectedPolitician}
+        onSelect={() =>
+          navigation.navigate("Profile", { data, selectedPolitician })
+        }
+        boxStyles={styles.boxStyle}
+        dropdownStyles={styles.dropdownStyle}
+        dropdownTextStyles={styles.dropdownTextStyle}
+        dropdownItemStyles={styles.dropdownItemStyles}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: 70,
-    },
-    button: {
-        backgroundColor: '#000',
-        paddingTop: 8,
-        paddingBottom: 8,
-        width: '70%',
-        borderRadius: 20,
-    },
-    searchText: {
-        color: '#fff',
-        textAlign: 'center',
-    },
+  boxStyle: {
+    width: "80%",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    padding: 0,
+  },
+  dropdownStyle: {},
+  dropdownItemStyles: {},
+  dropdownTextStyle: {
+    fontSize: 20,
+  },
+  button: {
+    backgroundColor: "#000",
+    paddingTop: 8,
+    paddingBottom: 8,
+    width: "70%",
+    borderRadius: 20,
+  },
+  searchText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  searchBar: {
+    width: "100%",
+    backgroundColor: "#eee",
+  },
+  containerSearchBar: {
+    backgroundColor: "#8496f7",
+  },
 });
