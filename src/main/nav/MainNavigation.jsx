44b5949342908f767430ec5690548aff79d9a823
MetaSoc <pk303187@student.polsl.pl>
2024-11-19 21:08:52 +0100

    Dodanie ikon do głównej nawigacji

--- src/main/nav/MainNavigation.jsx
+++ src/main/nav/MainNavigation.jsx
@@ -1,10 +1,13 @@
-import { useRoute } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {useRoute} from "@react-navigation/native";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import HomeScreen from "../home/HomeScreen";
 import ElectionNavigation from "../election/nav/ElectionNavigation";
 import SearchNavigation from "../search/nav/SearchNavigation";
 import TrendingScreen from "../trending/TrendingScreen";
 import ExtrasNavigation from "../extras/nav/ExtrasNavigation";
+import {Icon} from "react-native-paper";
+
+
 
 const Tab = createBottomTabNavigator();
 
@@ -17,56 +20,80 @@ export default function MainNavigation() {
       <Tab.Screen
         name="SearchNav"
         component={SearchNavigation}
-        initialParams={{ _title }}
-        options={() => ({
+        initialParams={{_title}}
+        options={{
           title: "Wyszukaj",
           headerShown: false,
           gestureEnabled: false,
-        })}
+          tabBarIcon: () => (
+            <Icon source="account-search" size={36}/>
+          ),
+          tabBarIconStyle: {top: 1},
+          // tabBarShowLabel: false,
+          // tabBarBadge: "+1"
+        }}
       />
       <Tab.Screen
         name="Election"
         component={ElectionNavigation}
-        options={() => ({
+        options={{
           title: "Wybory",
           headerTitle: _title,
           headerTitleAlign: "center",
           headerLeft: () => null,
           gestureEnabled: false,
-        })}
+          tabBarIcon: () => (
+            // <Icon source="draw" size={36}/>
+            <Icon source="email-newsletter" size={32}/>
+          ),
+          tabBarIconStyle: {top: 4}
+        }}
       />
       <Tab.Screen
         name="Home"
         component={HomeScreen}
-        options={() => ({
+        options={{
           title: "Strona główna",
           headerTitle: _title,
           headerTitleAlign: "center",
           headerLeft: () => null,
           gestureEnabled: false,
-        })}
+          tabBarIcon: () => (
+            <Icon source="home" size={36}/>
+          ),
+          tabBarIconStyle: {top: 1},
+
+        }}
       />
       <Tab.Screen
         name="Trending"
         component={TrendingScreen}
-        options={() => ({
+        options={{
           title: "Na czasie",
           headerTitle: _title,
           headerTitleAlign: "center",
           headerLeft: () => null,
           gestureEnabled: false,
-        })}
+          tabBarIcon: () => (
+            <Icon source="trending-up" size={36}/>
+          ),
+          tabBarIconStyle: {top: 1},
+        }}
       />
       <Tab.Screen
         name="ExtrasNav"
         component={ExtrasNavigation}
-        options={() => ({
+        options={{
           title: "Więcej",
           headerTitle: _title,
           headerTitleAlign: "center",
           headerLeft: () => null,
           gestureEnabled: false,
-        })}
+          tabBarIcon: () => (
+            <Icon source="menu" size={36}/>
+          ),
+          tabBarIconStyle: {top: 2},
+        }}
       />
     </Tab.Navigator>
   );
