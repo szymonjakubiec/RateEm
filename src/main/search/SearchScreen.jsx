19399ef6e6d028378945c9e9656cd3eb9efe9b10
MetaSoc <pk303187@student.polsl.pl>
2024-11-25 13:29:49 +0100

    Szybka naprawa wielokrotnego wywoływania ekranu

--- src/main/search/SearchScreen.jsx
+++ src/main/search/SearchScreen.jsx
@@ -1,21 +1,18 @@
 import {
-  Alert,
   StyleSheet,
-  Text,
-  TouchableHighlight,
   View,
 } from "react-native";
 import {
-  useCallback,
   useContext,
   useEffect,
-  useLayoutEffect,
   useState,
 } from "react";
-import { PoliticianNameContext } from "./PoliticianNameContext.jsx";
+import {PoliticianNameContext} from "./PoliticianNameContext.jsx";
 import SearchFlatList from "./searchScreenComponents/SearchFlatList.jsx";
 
-export default function SearchScreen({ navigation }) {
+
+
+export default function SearchScreen({navigation}) {
   const politicianNameData = useContext(PoliticianNameContext);
   const [selectedPoliticianId, setSelectedPoliticianId] = useState(0);
 
@@ -25,12 +22,12 @@ export default function SearchScreen({ navigation }) {
 
   useEffect(() => {
     setSelectedPoliticianId(0);
-  });
+  }, []);
   /**
    * Navigation to the ProfileScreen.js after selection of politician.
    */
   useEffect(() => {
-    console.log(selectedPoliticianId);
+    console.log("SearchScreen ~ politicianId:", selectedPoliticianId);
     if (selectedPoliticianId > 0) {
       navigation.navigate("Profile", {
         selectedPoliticianId,
@@ -40,7 +37,7 @@ export default function SearchScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <SearchFlatList data={politicianNameData} handleOnPress={handlePress} />
+      <SearchFlatList data={politicianNameData} handleOnPress={handlePress}/>
     </View>
   );
 }
