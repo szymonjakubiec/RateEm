6a85956e966a1eeeb96feb4f83be65b9eb79ef9d
szymonjakubiec <99356886+szymonjakubiec@users.noreply.github.com>
2024-11-27 21:45:54 +0100

    Usunięcie ClearTextInputButton i pare drobnych zmian

--- App.jsx
+++ App.jsx
@@ -16,13 +16,13 @@ const Stack = createStackNavigator();
 // PK
 // global.SERVER_URL = "http://10.10.17.22:3000/api"; // ms
 // global.SERVER_URL = "http://157.158.168.62:3000/api"; // aka
-global.SERVER_URL = "http://192.168.1.20:3000/api"; // cn
+// global.SERVER_URL = "http://192.168.1.20:3000/api"; // cn
 
 // MS
 // global.SERVER_URL = "http://157.158.168.61:3000/api"; // aka
 
 // MM
-// global.SERVER_URL = "http://10.0.2.2:3000/api"; // emu
+global.SERVER_URL = "http://10.0.2.2:3000/api"; // emu
 
 
 console.log(global.SERVER_URL);
