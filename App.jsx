d46d8bcbae3b27753564714d8c47c2c3866f3b16
Myetekk <mirekmich19@gmail.com>
2024-11-28 20:17:01 +0100

    ratings dates fixed

--- App.jsx
+++ App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import {createStackNavigator} from "@react-navigation/stack";
-import {NavigationContainer} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 import LoggingScreen from "./src/main/login/LoggingScreen";
 import MainNavigation from "./src/main/nav/MainNavigation";
 import RegisterNavigation from "./src/main/login/register/nav/RegisterNavigation";
-import {PaperProvider} from "react-native-paper";
+import { PaperProvider } from "react-native-paper";
 import ResetPassNavigation from "./src/main/login/resetPass/nav/ResetPassNavigation";
-
-
 
 const Stack = createStackNavigator();
 
@@ -16,17 +14,15 @@ const Stack = createStackNavigator();
 // PK
 // global.SERVER_URL = "http://10.10.17.22:3000/api"; // ms
 // global.SERVER_URL = "http://157.158.168.62:3000/api"; // aka
-global.SERVER_URL = "http://192.168.1.20:3000/api"; // cn
+// global.SERVER_URL = "http://192.168.1.20:3000/api"; // cn
 
 // MS
 // global.SERVER_URL = "http://157.158.168.61:3000/api"; // aka
 
 // MM
-// global.SERVER_URL = "http://10.0.2.2:3000/api"; // emu
-
+global.SERVER_URL = "http://10.0.2.2:3000/api"; // emu
 
 console.log(global.SERVER_URL);
-
 
 export default function App() {
   return (
