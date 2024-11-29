110ef4cd087abaf85a85ef3b86ed2c81ce5c643a
Meiczek <164068238+Meiczek@users.noreply.github.com>
2024-11-29 11:03:19 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -16,10 +16,11 @@ const Stack = createStackNavigator();
 // PK
 // global.SERVER_URL = "http://10.10.17.22:3000/api"; // ms
 // global.SERVER_URL = "http://157.158.168.62:3000/api"; // aka
-global.SERVER_URL = "http://192.168.1.20:3000/api"; // cn
+// global.SERVER_URL = "http://192.168.1.20:3000/api"; // cn
 
 // MS
-// global.SERVER_URL = "http://157.158.168.61:3000/api"; // aka
+//global.SERVER_URL = "http://157.158.168.61:3000/api"; // aka
+global.SERVER_URL = "http://192.168.1.136:3000/api"; // home
 
 // MM
 // global.SERVER_URL = "http://10.0.2.2:3000/api"; // emu
