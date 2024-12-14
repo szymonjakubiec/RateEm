35d8266c658aef69661072e3961710061be55cdb
MetaSoc <pk303187@student.polsl.pl>
2024-12-14 18:36:59 +0100

    Zmiany stylu Bottom Tab Navigation
    - Naprawa pokazywania dolnego paska przy wracaniu do głównej nawigacji
    - Małe czyszczenie kodu w Main Navigation

--- src/main/nav/MainNavigation.jsx
+++ src/main/nav/MainNavigation.jsx
@@ -1,54 +1,61 @@
-import { useEffect, useState } from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {useEffect, useState} from "react";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import ElectionNavigation from "../election/nav/ElectionNavigation";
 import HomeNavigation from "../home/nav/HomeNavigation";
-import { getAllPoliticians } from "../../backend/database/Politicians.js";
-import { GlobalContext } from "./GlobalContext.jsx";
+import {getAllPoliticians} from "../../backend/database/Politicians.js";
+import {GlobalContext} from "./GlobalContext.jsx";
 import ExtrasNavigation from "../extras/nav/ExtrasNavigation";
-import { Icon } from "react-native-paper";
+import {Icon, useTheme} from "react-native-paper";
+
+
 
 const Tab = createBottomTabNavigator();
 
-export default function MainNavigation({ route }) {
-  const { _title, userId } = route.params;
-  const [namesData, setNamesData] = useState();
+export default function MainNavigation({route}) {
+  const {_title, userId} = route.params;
+  const [namesData, setNamesData] = useState([]);
   const [updateDataTrigger, setUpdateDataTrigger] = useState(true);
 
   /**
-   * Asynchronously gets names of all politicians and passes it to the namesData.
+   * Initialization - Asynchronously gets names of all politicians and passes it to the namesData.
    * @async
    */
-  async function init() {
-    const data = await getAllPoliticians("surname", false);
-    setNamesData(data);
-  }
-
   useEffect(() => {
-    init();
+    (async () => {
+      const data = await getAllPoliticians("surname", false);
+      setNamesData(data);
+    })();
   }, []);
 
+  const theme = useTheme();
+
   return (
-    <GlobalContext.Provider value={{ namesData, setNamesData, userId, updateDataTrigger, setUpdateDataTrigger }}>
+    <GlobalContext.Provider value={{namesData, setNamesData, userId, updateDataTrigger, setUpdateDataTrigger}}>
       <Tab.Navigator
         backBehavior="initialRoute"
         initialRouteName="Home"
+        activeColor="red"
         screenOptions={{
           tabBarShowLabel: false,
           unmountOnBlur: true,
           tabBarHideOnKeyboard: true,
-          tabBarActiveBackgroundColor: "#00000012",
-          tabBarInactiveBackgroundColor: "#00000002",
+          tabBarActiveTintColor: theme.colors.primary,
+          // tabBarActiveBackgroundColor: theme.colors.primaryContainer,
+          tabBarActiveBackgroundColor: "#00000016",
+          tabBarInactiveTintColor: theme.colors.secondary,
+          tabBarInactiveBackgroundColor: theme.colors.background,
           tabBarStyle: {
-            height: 45,
+            height: "5.5%",
             borderTopLeftRadius: 10,
             borderTopRightRadius: 10,
           },
           tabBarItemStyle: {
-            borderRadius: 5,
+            borderRadius: 8,
             paddingTop: 5,
             paddingBottom: 10,
           },
         }}
+
       >
         <Tab.Screen
           name="Election"
@@ -58,11 +65,11 @@ export default function MainNavigation({ route }) {
             headerShown: false,
             headerTitleAlign: "center",
             gestureEnabled: false, // wyłącza swipe back na IOS
-            tabBarIcon: () => (
+            tabBarIcon: ({color}) => (
               // <Icon source="draw" size={36}/>
-              <Icon source="email-newsletter" size={32} />
+              <Icon color={color} source="email-newsletter" size={32}/>
             ),
-            tabBarIconStyle: { top: 4 },
+            tabBarIconStyle: {top: 4},
           }}
         />
         <Tab.Screen
@@ -74,8 +81,8 @@ export default function MainNavigation({ route }) {
             headerTitleAlign: "center",
             headerShown: false,
             gestureEnabled: false, // wyłącza swipe back na IOS
-            tabBarIcon: () => <Icon source="home" size={36} />,
-            tabBarIconStyle: { top: 1 },
+            tabBarIcon: ({color}) => <Icon color={color} source="home" size={36}/>,
+            tabBarIconStyle: {top: 1},
           }}
         />
         <Tab.Screen
@@ -87,8 +94,8 @@ export default function MainNavigation({ route }) {
             headerTitleAlign: "center",
             headerLeft: () => null,
             gestureEnabled: false, // wyłącza swipe back na IOS
-            tabBarIcon: () => <Icon source="menu" size={36} />,
-            tabBarIconStyle: { top: 2 },
+            tabBarIcon: ({color}) => <Icon color={color} source="menu" size={36}/>,
+            tabBarIconStyle: {top: 2},
           }}
         />
       </Tab.Navigator>
