8d9586fe673e78e3d0dbcdceb910972f22715a30
MetaSoc <pk303187@student.polsl.pl>
2024-12-15 22:50:03 +0100

    Dodanie obsługi ikon i multiline w _Button

--- src/main/styles/Button.jsx
+++ src/main/styles/Button.jsx
@@ -1,5 +1,5 @@
-import {StyleSheet, Text, TouchableOpacity, StyleProp, ViewStyle, TextStyle} from 'react-native';
-import {useTheme} from "react-native-paper";
+import {StyleSheet, Text, TouchableOpacity, StyleProp, ViewStyle, TextStyle, View} from 'react-native';
+import {Icon, useTheme} from "react-native-paper";
 
 
 
@@ -10,6 +10,15 @@ import {useTheme} from "react-native-paper";
  * @param {StyleProp<TextStyle>} [textStyle] - additional text styles.
  * @param {'standard'|'onlyText'|'tile'} [mode='standard'] - button's mode.
  * @param {StyleProp<ViewStyle>} [style] - additional button styles.
+ * @param {object} [iconLeft] - icon to display on the left of text.
+ * @param {string} iconLeft.icon - icon to display on the left of text.
+ * @param {number} [iconLeft.size=24] - size of icon.
+ * @param {string} [iconLeft.color="white"] - color of icon.
+ * @param {object} [iconRight] - icon to display on the right of text.
+ * @param {string} iconRight.icon - icon to display on the right of text.
+ * @param {number} [iconRight.size=24] - size of icon.
+ * @param {string} [iconRight.color="white"] - color of icon.
+ * @param {boolean} [multiline=false] - if text should be on multiple lines.
  * @param {boolean} [disabled='false'] - if button is disabled.
  * @param [props]
  * @returns {JSX.Element}
@@ -20,6 +29,9 @@ export default function _Button({
                                   textStyle,
                                   mode = "standard",
                                   style,
+                                  iconLeft,
+                                  iconRight,
+                                  multiline = false,
                                   disabled = false,
                                   ...props
                                 }) {
@@ -52,7 +64,7 @@ export default function _Button({
       fontSize: mode === "tile" ? 18 : 16,
       textShadowColor: '#00000066',
       textShadowOffset: {width: 1, height: 1},
-      textShadowRadius: 4
+      textShadowRadius: 4,
     },
     onlyText: {
       fontSize: 14,
@@ -62,15 +74,40 @@ export default function _Button({
     }
   });
 
+  iconLeft = {
+    icon: "",
+    color: useTheme().colors.onPrimary,
+    size: 26,
+    ...iconLeft
+  };
+
+  iconRight = {
+    icon: "",
+    color: useTheme().colors.onPrimary,
+    size: 26,
+    ...iconRight
+  };
+
+
   return (
     mode === "standard" || mode === "tile" ?
       <TouchableOpacity
         activeOpacity={0.8}
         disabled={disabled}
-        style={[styles.button, styles.disabled, style]}
+        style={[styles.button, styles.disabled, style, iconLeft.icon !== "" && {paddingLeft: "6%"}]}
         onPress={onPress}
         {...props}>
-        <Text numberOfLines={1} adjustsFontSizeToFit={true} style={[styles.buttonText, textStyle]}> {text} </Text>
+        <View style={{flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
+          {iconLeft.icon ? (
+            <Icon color={iconLeft.color} size={iconLeft.size} source={iconLeft.icon}/>
+          ) : null}
+          <Text numberOfLines={multiline ? 0 : 1} adjustsFontSizeToFit={!multiline}
+                style={[styles.buttonText, textStyle, iconLeft.icon !== "" && {paddingLeft: "5%"},
+                  iconRight.icon !== "" && {paddingRight: "5%"}, multiline && {textAlign: "left"}]}>{text}</Text>
+          {iconRight.icon ? (
+            <Icon color={iconRight.color} size={iconRight.size} source={iconRight.icon}/>
+          ) : null}
+        </View>
       </TouchableOpacity>
       : mode === "onlyText" ?
         <TouchableOpacity
